57b1781854a06417cd3f46563b65a101e103513b
Nolan Crooks <nolancrooks@outlook.com>
2022-01-29 17:59:44 -0800

    move prometheus connection, add initial promql request

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,11 @@ import logging
 import logging.config
 import requests
 import connexion
-from datetime import datetime
-from prometheus_api_client import PrometheusConnect, PrometheusApiClientException
 from flask_cors import CORS, cross_origin
+from datetime import datetime, timedelta
+
+from prometheus_api_client import PrometheusConnect, PrometheusApiClientException, MetricsList, Metric, MetricRangeDataFrame
+from prometheus_api_client.utils import parse_datetime
 
 # set up logging and app configuration
 app_conf_file = "app_conf.yml"
@@ -20,9 +22,18 @@ with open(log_conf_file, 'r') as f:
     logging.config.dictConfig(log_config)
 logger = logging.getLogger('basicLogger')
 
+# connect to prometheus
+logger.info(f"Attempting to connect to Prometheus at http://{app_config['prometheus']['host']}...")
+try:
+    prom = PrometheusConnect(url=f"http://{app_config['prometheus']['host']}", disable_ssl=True)
+except Error as e:
+    logger.info(f"Failed to connect to Prometheus at http://{app_config['prometheus']['host']}. Please check your connection details and try again. Error:")
+    logger.info(e)
+    exit(1)
+logger.info(f"Connection to Prometheus succeeded.")
+
 def startup():
-    """ Connects to Prometheus TSDB and starts Flask App """
-    connect_prometheus()
+    """ Configures and starts Flask App """
     app = connexion.FlaskApp(__name__, specification_dir='')
     app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
     CORS(app.app)
@@ -31,23 +42,54 @@ def startup():
     logger.info(f"Backend running on localhost:{app_config['app']['port']}")
     logger.info(f"Make requests at http://localhost:{app_config['app']['port']}/ui")
     
-    app.run(port=app_config['app']['port'], use_reloader=False, debug=False)
+    app.run(port=app_config['app']['port'], use_reloader=True, debug=True)
 
-def connect_prometheus():
-    """ Attempts connection with Prometheus """
-    global prom
-    logger.info(f"Attempting to connect to Prometheus at http://{app_config['prometheus']['host']}...")
-    try:
-        prom = PrometheusConnect(url=f"http://{app_config['prometheus']['host']}", disable_ssl=True)
-    except Error as e:
-        logger.info(f"Failed to connect to Prometheus at http://{app_config['prometheus']['host']}. Please check your connection details and try again. Error:")
-        logger.info(e)
-        exit(1)
-    logger.info(f"Connection to Prometheus succeeded.")
-
-def get_traceroutes(body):
+def get_traceroutes(src, dest, start, end):
     """ Retrieves array of traceroutes from Prometheus """
     logger.info(f'Getting traceroutes')
+
+    traceroutes = []
+    hops = []
+
+    start_time = parse_datetime(start)
+    end_time = parse_datetime(end)
+    chunk_size = timedelta(hours=1)
+
+    instance = src
+    target = dest
+
+
+    # TODO:
+    # Generate PromQL query from query params
+    metric = 'mtr_rtt_seconds'
+    label_config = {
+        'instance': instance,
+        'target': target, 
+        'type': 'mean'
+        }
+    tracert_metric_range_data = prom.get_metric_range_data(
+        metric_name=metric,
+        label_config=label_config,
+        start_time=start_time,
+        end_time=end_time,
+        chunk_size=chunk_size,
+    )
+
+    # traceroutes_metric_range_data = MetricsList(metric_data) # metric_object_list will be initialized as
+                                                # a list of Metric objects for all the
+                                                # metrics downloaded using get_metric query
+
+    # We can see what each of the metric objects look like
+    # for item in traceroutes_metricslist:
+    #     print(item, "\n")
+    #     hops += [{'ttl': int(item.label_config['ttl']), 'host': item.label_config['path'], 'trace_time': '2016-08-29T09:12:33.001Z'}]
+
+    tracert_df = MetricRangeDataFrame(tracert_metric_range_data)
+    print('traceroute:')
+    tracert_df.head()
+
+    # Parse TTLs into something we can make sense of for each traceroute on the graph
+    print(traceroutes)
 
     return traceroutes, 200
 
