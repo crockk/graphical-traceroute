8685aa7d91bba40f20b4727a030b107cb6dbceda
Nolan Crooks <nolancrooks@outlook.com>
2022-01-29 21:42:31 -0800

    Finish get traceroutes function

--- backend/app.py
+++ backend/app.py
@@ -47,7 +47,6 @@ def startup():
 
 def process_duration(duration, end_time):
     """ Gets date based on a given duration and end time"""
-    print(duration)
     if duration[-1] == 's':
         duration = int(duration[:-1])
     elif duration[-1] == 'm':
@@ -58,7 +57,6 @@ def process_duration(duration, end_time):
         duration = int(duration[:-1]) * 86400
     else:
         raise ValueError(f"Time unit {duration[-1]} is not supported. The following units are supported: s, m, h, d")
-    print(f'converted duration: {duration}')
     return datetime.fromtimestamp((datetime.timestamp(end_time) - duration))
 
 def get_traceroutes(src, dest, search_duration, end_time, num_tracert):
@@ -66,22 +64,14 @@ def get_traceroutes(src, dest, search_duration, end_time, num_tracert):
     logger.info(f'Getting traceroutes')
 
     traceroutes = []
-    hops = []
 
     end_time = parse_datetime(end_time)
     start_time = process_duration(search_duration, end_time)
     interval_seconds = ((end_time - start_time).seconds / num_tracert)
-    # print('start time:')
-    # print(start_time)
-    # print('end time:')
-    # print(end_time)
-
 
     instance = src
     target = dest
 
-
-    # TODO:
     # Generate PromQL query from query params
     metric = 'mtr_rtt_seconds'
     label_config = {
@@ -97,30 +87,20 @@ def get_traceroutes(src, dest, search_duration, end_time, num_tracert):
         end_time=end_time,
     )
 
-    traceroutes_metric_list_data = MetricsList(tracert_metric_range_data) # metric_object_list will be initialized as
-                                                # a list of Metric objects for all the
-                                                # metrics downloaded using get_metric query
+    # Generate dataframe for data processing
+    tracert_df = MetricRangeDataFrame(tracert_metric_range_data)
 
-    # We can see what each of the metric objects look like
-    # for item in traceroutes_metric_list_data:
-    #     print(item, "\n")
-    #     hops += [{'ttl': int(item.label_config['ttl']), 'host': item.label_config['path'], 'trace_time': '2016-08-29T09:12:33.001Z'}]
-
-    # tracert_df = MetricRangeDataFrame(tracert_metric_range_data)
-    # print('traceroute:')
-    # print(tracert_df.head())
-    # print(tracert_df.index)
-
-    # print('rows at timestamp 1643511196.308')
-    # print(tracert_df.loc[1643511196.308])
-
-    # current_ts = tracert_df.index.max()
-    # for i in range(num_tracert):
-    #     print(tracert_df.loc[current_ts])
-    #     current_ts = current_ts - interval_seconds
-
-    # Parse TTLs into something we can make sense of for each traceroute on the graph
-    # print(traceroutes)
+    this_timestamp = tracert_df.index.max()
+    for i in range(num_tracert):
+        hops = []
+        hop_vals = tracert_df.loc[this_timestamp, ["path", "ttl"]].values
+        for hop in hop_vals:
+            this_hop = {}
+            this_hop['host'] = hop[0]
+            this_hop['ttl'] = int(hop[1])
+            hops.append(this_hop)
+        traceroutes.append({'hops': hops, 'trace_time': datetime.fromtimestamp(this_timestamp)})
+        this_timestamp = this_timestamp - interval_seconds
 
     return traceroutes, 200
 
