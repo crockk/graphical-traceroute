19d8accf6432b88227ceb11bd7f0cd60067d61c7
Tushya Iyer <tushyais@gmail.com>
2022-01-31 03:22:06 -0800

    remove tempalte code. add top bar and night mode switch

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,80 @@
 <script lang="ts">
-	export let name: string;
+
+  import Button from '@smui/button';
+  import TopAppBar, {
+		Row,
+		Section,
+		Title,
+		AutoAdjust,
+		TopAppBarComponentDev
+	} from '@smui/top-app-bar';
+  import { Label, Icon } from '@smui/common';
+
+  import GraphTraceRoute from './graphical-traceroute/GraphTraceRoute.svelte'
+
+  let lightTheme = typeof window === 'undefined' || window.matchMedia('(prefers-color-scheme: light)').matches;
+  $: console.log((lightTheme ? "light" : "dark") + " theme activated")
+
+  function switchTheme() {
+    lightTheme = !lightTheme;
+    let themeLink = document.head.querySelector<HTMLLinkElement>('#theme');
+    if (!themeLink) {
+      themeLink = document.createElement('link');
+      themeLink.rel = 'stylesheet';
+      themeLink.id = 'theme';
+    }
+    themeLink.href = `/static/smui${lightTheme ? '' : '-dark'}.css`;
+    document.head.querySelector<HTMLLinkElement>('link[href="/static/smui-dark.css"]')
+    ?.insertAdjacentElement('afterend', themeLink);
+
+
+    let siteThemeLink = document.head.querySelector<HTMLLinkElement>('#site');
+    if (!siteThemeLink) {
+      siteThemeLink = document.createElement('link');
+      siteThemeLink.rel = 'stylesheet';
+      siteThemeLink.id = 'site';
+    }
+    siteThemeLink.href = `/static/site${lightTheme ? '' : '-dark'}.css`;
+    document.head.querySelector<HTMLLinkElement>('link[href="/static/site-dark.css"]')
+    ?.insertAdjacentElement('afterend', siteThemeLink);
+
+  }
+
+  let topAppBar: TopAppBarComponentDev;
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<TopAppBar bind:this={topAppBar} class="demo-top-app-bar mdc-top-app-bar smui-top-app-bar--static" >
+  <Row>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    <Section>
+      <Title class="mdc-theme--primary" style="font-weight: bold">Graphical Trace Route</Title>
+    </Section>
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+    <Section>
+    <!--
+      <TabBar tabs={webpages} let:tab bind:active={activeTab}>
+        <Tab class="secondary"  {tab}>
+          <Label>{tab.name}</Label>
+        </Tab>
+      </TabBar>
+      -->
+    </Section>
+
+    <Section align="end" toolbar>
+      <Button color='secondary' variant="raised" toggle on:click={switchTheme}>
+        <Icon class="material-icons">{ lightTheme ? "light_mode" : "dark_mode" }</Icon>
+        <Label>{lightTheme ? 'Lights off' : 'Lights on'}</Label>
+      </Button>
+    </Section>
+
+  </Row>
+</TopAppBar>
+
+<AutoAdjust {topAppBar}>
+</AutoAdjust>
+
+<div>
+  <GraphTraceRoute/>
+</div>
