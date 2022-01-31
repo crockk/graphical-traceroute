1144702086fb9fce8e80da8962858d8cfd32b470
Tushya Iyer <tushyais@gmail.com>
2022-01-31 03:22:51 -0800

    add rough src, dst, max route input fields queried from backend

---
+++ frontend/src/graphical-traceroute/GraphTraceRoute.svelte
@@ -0,0 +1,20 @@
+<script>
+
+  import QueryInput from './QueryInput.svelte'
+  let srcNode;
+  let destNode;
+  let numRoutes;
+
+</script>
+
+<QueryInput
+  bind:srcNode={srcNode}
+  bind:destNode={destNode}
+  bind:numRoutes={numRoutes}
+/>
+
+
+
+{srcNode}
+{destNode}
+{numRoutes}
---
+++ frontend/src/graphical-traceroute/QueryInput.svelte
@@ -0,0 +1,106 @@
+<script>
+  import Button from '@smui/button';
+  import { Label } from '@smui/common';
+  import Select, { Option } from '@smui/select';
+  import Slider from '@smui/slider';
+  import FormField from '@smui/form-field';
+  import Icon from '@smui/select/icon';
+
+  const backendBaseURL = "http://localhost:8100"
+
+  let promiseMaxRoutes;
+  let promiseSrcNodes;
+  let promiseDestNodes;
+
+  function getMaxRoutes() {
+    promiseMaxRoutes = fetch(backendBaseURL + '/max-routes', {
+      mode: 'cors',
+    }).then((x) => x.json());
+  };
+
+  function getSrcNodes() {
+    promiseSrcNodes = fetch(backendBaseURL + '/src', {
+      mode: 'cors',
+    }).then((x) => x.json());
+  };
+
+  function getDestNodes() {
+    promiseDestNodes = fetch(backendBaseURL + '/dest', {
+      mode: 'cors',
+    }).then((x) => x.json());
+  };
+
+  export let srcNode;
+  export let destNode;
+  export let numRoutes = 1;
+
+  getMaxRoutes();
+  getSrcNodes();
+  getDestNodes();
+
+</script>
+
+
+<Button variant="raised"
+    on:click={getMaxRoutes}
+    on:click={getSrcNodes}
+    on:click={getDestNodes}
+  >
+  <Icon class="material-icons">refresh</Icon>
+  <Label>Update route data</Label>
+</Button>
+
+
+<Select
+  class="shaped-outlined"
+  variant="outlined"
+  bind:value={srcNode}
+  label="Source Node"
+  required
+>
+  <Icon class="material-icons" slot="leadingIcon">computer</Icon>
+  <!-- <Option value="" /> -->
+
+  {#await promiseSrcNodes then srcNodes}
+    {#if srcNodes}
+      {#each srcNodes as sNode}
+        <Option value={sNode}>{sNode}</Option>
+      {/each}
+    {/if}
+  {/await}
+</Select>
+
+
+<Select
+  class="shaped-outlined"
+  variant="outlined"
+  bind:value={destNode}
+  label="Destination Node"
+  required
+>
+  <Icon class="material-icons" slot="leadingIcon">dns</Icon>
+  <!-- <Option value="" /> -->
+
+  {#await promiseDestNodes then destNodes}
+    {#if destNodes}
+      {#each destNodes as dNode}
+        <Option value={dNode}>{dNode}</Option>
+      {/each}
+    {/if}
+  {/await}
+</Select>
+
+<div>
+{#await promiseMaxRoutes then maxRoutes}
+  {#if maxRoutes}
+    <Slider
+      bind:value={numRoutes}
+      min={1}
+      max={ maxRoutes.max_routes }
+      discrete
+      tickMarks
+      input$aria-label="Tick mark slider for max trace routes"
+    />
+  {/if}
+{/await}
+</div>
