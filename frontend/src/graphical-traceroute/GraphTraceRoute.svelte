c0143a2a308769eb9b26ca2b4e7fc472f319b2a9
Tushya Iyer <tushyais@gmail.com>
2022-02-02 02:06:53 -0800

    factor param summary and query submit to TraceRouteSubmit component

--- frontend/src/graphical-traceroute/GraphTraceRoute.svelte
+++ frontend/src/graphical-traceroute/GraphTraceRoute.svelte
@@ -2,6 +2,7 @@
 
   import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
   import TraceRouteParams from './TraceRouteParams.svelte'
+  import TraceRouteSubmit from './TraceRouteSubmit.svelte'
   import Paper, { Title, Content } from '@smui/paper';
   import {
     srcNode,
@@ -17,41 +18,4 @@
 
 <br>
 
-<Paper color="primary" variant="outlined" class="mdc-theme--primary">
-  <Title>Chosen Trace Route Query Parameters</Title>
-  <Content>
-
-    <DataTable table$aria-label="Traceroute query list" style="max-width: 100%;">
-      <Head>
-        <Row>
-          <Cell numeric>Number of Traceroutes</Cell>
-          <Cell>Source Node</Cell>
-          <Cell>Destination Node</Cell>
-          <Cell>Search Duration</Cell>
-          <Cell>Start Date</Cell>
-        </Row>
-      </Head>
-      <Body>
-        <Row>
-          <Cell>
-            {$numRoutes}
-          </Cell>
-          <Cell>
-            {$srcNode}
-          </Cell>
-          <Cell>
-            {$destNode}
-          </Cell>
-          <Cell>
-            {$searchDuration}
-          </Cell>
-          <Cell>
-            {$selectedDate}
-          </Cell>
-
-        </Row>
-      </Body>
-    </DataTable>
-
-  </Content>
-</Paper>
+<TraceRouteSubmit/>
---
+++ frontend/src/graphical-traceroute/TraceRouteSubmit.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+
+  import DataTable, { Head, Body, Row, Cell as DataCell } from '@smui/data-table';
+  import Paper, { Title, Content } from '@smui/paper';
+  import LayoutGrid, { Cell as LayoutCell, InnerGrid } from '@smui/layout-grid';
+  import Button, { Label } from '@smui/button';
+  import axios from "axios";
+  import {
+    srcNode,
+    destNode,
+    numRoutes,
+    searchDuration,
+    selectedDate
+  } from '../store.js'
+
+  const backendBaseURL = "http://localhost:8100"
+
+  export let traceroutes = [];
+
+  $: console.dir(traceroutes);
+
+  async function submitQuery() {
+
+    let params = {
+      src: $srcNode,
+      dest: $destNode,
+      search_duration: $searchDuration,
+      start_dt: $selectedDate,
+      num_tracert: $numRoutes
+    };
+
+    traceroutes = await axios.get(backendBaseURL + '/traceroute', { params: params }).then((x) => x.data);
+  };
+
+</script>
+
+
+<Paper color="primary" variant="outlined" class="mdc-theme--primary">
+  <Title>Chosen Trace Route Query Parameters</Title>
+  <Content>
+    <LayoutGrid>
+
+      <!-- ROW -->
+      <LayoutCell span="1">
+      </LayoutCell>
+
+      <LayoutCell span="8">
+
+        <DataTable table$aria-label="Traceroute query list" style="max-width: 100%;">
+          <Head>
+            <Row>
+              <DataCell numeric>Number of Traceroutes</DataCell>
+              <DataCell>Source Node</DataCell>
+              <DataCell>Destination Node</DataCell>
+              <DataCell>Search Duration</DataCell>
+              <DataCell>Start Date</DataCell>
+            </Row>
+          </Head>
+          <Body>
+            <Row>
+              <DataCell>
+                {$numRoutes}
+              </DataCell>
+              <DataCell>
+                {$srcNode}
+              </DataCell>
+              <DataCell>
+                {$destNode}
+              </DataCell>
+              <DataCell>
+                {$searchDuration}
+              </DataCell>
+              <DataCell>
+                {$selectedDate}
+              </DataCell>
+
+            </Row>
+          </Body>
+        </DataTable>
+
+      </LayoutCell>
+
+      <LayoutCell span="3">
+        <Button variant="raised"
+            on:click={submitQuery}
+          >
+          <!-- <Icon class="material-icons">refresh</Icon> -->
+          <Label>Submit Query</Label>
+        </Button>
+      </LayoutCell>
+  </LayoutGrid>
+  </Content>
+</Paper>
