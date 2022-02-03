559e365e3b96a870ec401650b7b54bbd019684d1
Tushya Iyer <tushyais@gmail.com>
2022-02-03 02:36:12 -0800

    add tab bar for text vs graph results.

--- frontend/src/graphical-traceroute/TraceRouteResults.svelte
+++ frontend/src/graphical-traceroute/TraceRouteResults.svelte
@@ -1,15 +1,50 @@
 <script lang="ts">
   import Paper, { Title, Content } from '@smui/paper';
+  import Tab, { Label } from '@smui/tab';
+  import TabBar from '@smui/tab-bar';
+  import LayoutGrid, { Cell } from '@smui/layout-grid';
 
   import TraceRouteResultsText from './TraceRouteResultsText.svelte'
+
+  const resultPages = [
+    { id: 1,
+      component: TraceRouteResultsText,
+      title: "Text Results",
+    },
+    { id: 2,
+      component: TraceRouteResultsText,
+      title: "Graph Results",
+    }
+  ];
+
+  let activeTabItem = resultPages[0];
 
 </script>
 
 <Paper color="primary" variant="outlined" class="mdc-theme--primary">
-  <Title>Trace Route Results</Title>
+
+  <Title>
+    <LayoutGrid>
+
+      <Cell span="3">
+        Trace Route Results
+      </Cell>
+
+      <Cell span="9">
+
+        <TabBar tabs={resultPages} let:tab bind:active={activeTabItem}>
+          <Tab {tab}>
+            <Label>{tab.title}</Label>
+          </Tab>
+        </TabBar>
+
+      </Cell>
+
+    </LayoutGrid>
+  </Title>
+
   <Content>
-
-    <TraceRouteResultsText/>
-
+    <svelte:component this={activeTabItem.component} />
   </Content>
+
 </Paper>
