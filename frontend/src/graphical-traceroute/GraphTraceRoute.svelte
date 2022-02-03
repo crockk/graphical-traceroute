74b172f69500cce9ab98a3953442488709da0a88
Tushya Iyer <tushyais@gmail.com>
2022-02-03 00:56:13 -0800

    remove unnecessary imports

--- frontend/src/graphical-traceroute/GraphTraceRoute.svelte
+++ frontend/src/graphical-traceroute/GraphTraceRoute.svelte
@@ -1,18 +1,8 @@
 <script>
 
-  import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
   import TraceRouteParams from './TraceRouteParams.svelte'
   import TraceRouteSubmit from './TraceRouteSubmit.svelte'
   import TraceRouteResults from './TraceRouteResults.svelte'
-  import Paper, { Title, Content } from '@smui/paper';
-  import {
-    srcNode,
-    destNode,
-    numRoutes,
-    searchDuration,
-    tracerouteQueryResults,
-    selectedDate
-  } from '../store.js'
 
 </script>
 
